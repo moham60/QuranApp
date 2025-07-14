aaa78a82e58d0901f550baf5bd2f0caed05e465c
moham60 <alkingg289@gmail.com>
2025-07-14 21:21:50 +0300

    Updates

--- assets/index-2y31jrq0.js
+++ assets/index-2y31jrq0.js
@@ -1,4 +1,4 @@
-import{i as bn,R as Te}from"./index-CCTcjUeX.js";/*!
+import{k as bn,R as Te}from"./index-DOydZ2TE.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
