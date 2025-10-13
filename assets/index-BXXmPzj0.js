1650fe2eaa5402bc26c12feaf9bb6bd8fa96d543
moham60 <alkingg289@gmail.com>
2025-10-13 21:33:45 +0300

    Updates

--- assets/index-BXXmPzj0.js
+++ assets/index-BXXmPzj0.js
@@ -1,4 +1,4 @@
-import{k as bn,R as Te}from"./index-DCMJKyRT.js";/*!
+import{k as bn,R as Te}from"./index-BEEPO5o2.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
