1beb9cf3219b45159bbb63ad0793ee9979aab105
moham60 <alkingg289@gmail.com>
2025-10-22 01:33:43 +0300

    Updates

--- assets/index-DN5UMS5q.js
+++ assets/index-DN5UMS5q.js
@@ -1,4 +1,4 @@
-import{n as bn,R as Te}from"./index-OEO075A7.js";/*!
+import{n as bn,R as Te}from"./index-BFYfXr6H.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
